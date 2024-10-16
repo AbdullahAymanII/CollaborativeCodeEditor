5745e98f03173ccce5475747d4165fc97b4525a1
Abdullah Ayman <abdayman1092002@gmail.com>
2024-10-16 21:08:33 +0300

    5

--- editor/src/test/java/com/collaborative/editor/EditorApplicationTests.java
+++ editor/src/test/java/com/collaborative/editor/EditorApplicationTests.java
@@ -2,12 +2,13 @@ package com.collaborative.editor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // Add a test profile
 class EditorApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
+
